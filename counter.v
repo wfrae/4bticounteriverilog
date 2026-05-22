468f866eb4e4ad6711420991cbb527cfe51e1efb
erin <felicitytyler49@Gmail.com>
2026-05-22 09:21:45 -0600

    fix syntax errors in counter module and ensure proper formatting

--- counter.v
+++ counter.v
@@ -1,13 +1,16 @@
 module counter (input wire clk, 
                 input wire rst_n, 
-                output reg [3:0] count);
+                output reg [3:0] count
+);
 
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-      out <= 4'b0000
-      and else begin
-        out <= out + 1b1
+      out <= 4'b0000;
+    end 
+      else begin
+        out <= out + 1b1;
       end
+      
     end
 
 endmodule
