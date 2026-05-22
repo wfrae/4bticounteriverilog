d0a72db3278188dd5b861f1b30ce138370795597
erin <felicitytyler49@Gmail.com>
2026-05-22 09:26:29 -0600

    refactor counter module for improved readability and correct output variable usage

--- counter.v
+++ counter.v
@@ -1,16 +1,16 @@
-module counter (input wire clk, 
-                input wire rst_n, 
-                output reg [3:0] count
+module counter (
+    input wire clk, 
+    input wire rst_n, 
+    output reg [3:0] count
 );
 
-always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-      out <= 4'b0000;
-    end 
-      else begin
-        out <= out + 1b1;
-      end
-      
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            count <= 4'b0000;
+        end 
+        else begin
+            count <= count + 1'b1;
+        end
     end
 
 endmodule
