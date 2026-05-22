fd7a5fa6012eb095b027e39306a283e0924f5e95
erin <felicitytyler49@Gmail.com>
2026-05-22 09:18:21 -0600

    fix syntax errors and update counter module implementation

--- counter.v
+++ counter.v
@@ -1,10 +1,13 @@
-module (clk,
-        rst_n,
-        count);
+module counter (input wire clk, 
+                input wire rst_n, 
+                output reg [3:0] count);
 
 always @(posedge clk or negedge rst_n) begin
-    if (!rst_n)
-        count <= 0;
-    else
-        count <= count + 1;
-end
+    if (!rst_n) begin
+      out <= 4'b0000
+      and else begin
+        out <= out + 1b1
+      end
+    end
+
+endmodule
